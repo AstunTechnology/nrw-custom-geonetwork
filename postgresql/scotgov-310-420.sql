94ea20c8ced3d9325c0c80be9d96cabd6e82881f
archaeogeek <jo.k.cook@gmail.com>
2023-02-14 16:15:17 +0000

    updated sql steps for getting database to 4.0.1

--- postgresql/scotgov-310-420.sql
+++ postgresql/scotgov-310-420.sql
@@ -1,3 +1,12 @@
+# create database geonetwork420clean
+#\c geonetwork420clean
+# create extension hstore
+# create extension postgis
+# restore dump
+# run audit.sql
+
+
+
 delete from validation where metadataid in (select id from metadata where schemaid = 'dcat-ap');
 delete from metadata where schemaid = 'dcat-ap';
 delete from statusvaluesdes where iddes = 63;
@@ -9,3 +18,21 @@ delete from schematrondes where label = 'schematron-rules-GEMINI_2.3_supp-v1.0';
 
 update validation set valtype = 'schematron-rules-GEMINI_23_supp_v10' where valtype = 'schematron-rules-GEMINI_2.3_supp-v1.0';
 update validation set valtype = 'schematron-rules-GEMINI_23_v10' where valtype = 'schematron-rules-GEMINI_2.3_Schema-v1.0';
+
+/*DELETE 400
+DELETE 100
+DELETE 17
+DELETE 1
+DELETE 4
+DELETE 1
+DELETE 1
+UPDATE 371
+UPDATE 371*/
+
+/*select * from settings where name like '%version%';
++-------------------------+-------+----------+----------+----------+-----------+----------+
+| name                    | value | datatype | position | internal | encrypted | editable |
+|-------------------------+-------+----------+----------+----------+-----------+----------|
+| system/platform/version | 4.0.1 | 0        | 150      | n        | n         | y        |
++-------------------------+-------+----------+----------+----------+-----------+----------+*/
+
