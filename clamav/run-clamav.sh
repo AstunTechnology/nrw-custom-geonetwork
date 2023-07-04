47d79ea1fee1c7b81daf82245843adba880e6d26
Elena Robu <elenarobu@astuntechnology.com>
2023-07-04 12:26:45 +0000

    modifications to clamav to work with ssmtp rather than curl to send emails

--- clamav/run-clamav.sh
+++ clamav/run-clamav.sh
@@ -13,6 +13,12 @@ mkdir -p /home/astun/clamav-logs /home/astun/clamav-quarantine
 # unset $DOCKER_CONTENT_TRUST because the av container is not signed
 unset DOCKER_CONTENT_TRUST
 
+# set up output file from sample with environment variables and such like
+cp output.txt.sample output.txt
+sed -i -e "s|TOADDRESS|$EMAIL_ADDR|g" output.txt
+sed -i -e "s|FROMADDRESS|$EMAIL_ADDR|g" output.txt
+sed -i -e "s|SUBJECT|$GN_SITE_NAME antivirus output $(date +%Y-%m-%d)|g" output.txt
+
 # change the first mounted volume to match the correct directory to scan
 docker run --rm -v /var/lib/docker/volumes:/scan:ro -v /home/astun/clamav-logs:/logs:rw -v /home/astun/clamav-quarantine:/quarantine:rw tquinnelly/clamav-alpine --log=logs/output.txt --move=quarantine
 
@@ -21,14 +27,17 @@ if [ ! -f /home/astun/clamav-logs/output.txt ]; then
         echo -e "Antivirus job ran, but no output was generated\n" >> /home/astun/clamav-logs/output.txt
 fi
 
-# send email with output.txt as body
-curl -v --url smtps://$SMTP_URL_CLAMAV --ssl-reqd  --mail-from $EMAIL_ADDR --mail-rcpt $EMAIL_ADDR  --user $SMTP_USERNAME:$SMTP_PASSWORD -F '=</home/astun/clamav-logs/output.txt;encoder=quoted-printable' -H "Subject: $GN_SITE_NAME  antivirus output $(date +%Y-%m-%d)" -H "From: $EMAIL_ADDR <$EMAIL_ADDR>" -H "To: $EMAIL_ADDR <$EMAIL_ADDR>"
+# append logs to output email file
+cat /home/astun/clamav-logs/output.txt >> output.txt
 
-# remove the old log file
+# send email with output.txt as body
+#curl -v --url smtps://$SMTP_URL_CLAMAV --ssl-reqd  --mail-from $EMAIL_ADDR --mail-rcpt $EMAIL_ADDR  --user $SMTP_USERNAME:$SMTP_PASSWORD -F '=</home/astun/clamav-logs/output.txt;encoder=quoted-printable' -H "Subject: $GN_SITE_NAME  antivirus output $(date +%Y-%m-%d)" -H "From: $EMAIL_ADDR <$EMAIL_ADDR>" -H "To: $EMAIL_ADDR <$EMAIL_ADDR>"
+ssmtp metadata@astuntechnology.com < output.txt
+
+# remove the old log file and output email file
 rm /home/astun/clamav-logs/output.txt
+rm /home/astun/docker-geonetwork/clamav/output.txt
 
 # reset $DOCKER_CONTENT_TRUST
 export DOCKER_CONTENT_TRUST=1
-
-
 
